a8ee666c948260a5b5d831deab0e747802647de8
mohit-mhjn <mohitm3@illinois.edu>
2021-09-14 23:57:24 -0500

    estd depedencies with coinUtils and boost

---
+++ testCoinUtils.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<CoinMpsIO.hpp>
+#include<CoinLpIO.hpp>
+#include<boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main () {
+    
+    // CoinMpsIO::readMps()
+
+}
+
+
+
